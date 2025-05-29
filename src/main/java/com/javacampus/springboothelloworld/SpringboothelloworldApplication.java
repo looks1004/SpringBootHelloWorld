eb3311f6fcc04a372b253e0aea99bc869d1b2e7b
김호용 <hp7984@naver.com>
2025-05-29 11:32:38 +0900

    25.05.29 home

--- src/main/java/com/javacampus/springboothelloworld/SpringboothelloworldApplication.java
+++ src/main/java/com/javacampus/springboothelloworld/SpringboothelloworldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SpringboothelloworldApplication {
 
 
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String index(){
 		return "hello world....java index 우선";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboothelloworldApplication.class, args);
 	}
+
+
 
 }
